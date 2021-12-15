594809591272d0c2ce6cc26d98fa216428bc1da3
Philipe Silva Grigorio <phgrigorio@gmail.com>
2021-12-15 19:00:29 -0300

    Localization pt-BR para mensagens na ui

--- src/options.js
+++ src/options.js
@@ -294,13 +294,13 @@ let defaultOptions = {
   /**
    * The text used before any files are dropped.
    */
-  dictDefaultMessage: "Drop files here to upload",
+  dictDefaultMessage: "Arraste e solte os arquivos aqui para fazer upload",
 
   /**
    * The text that replaces the default message text it the browser is not supported.
    */
   dictFallbackMessage:
-    "Your browser does not support drag'n'drop file uploads.",
+    "Seu navegador não suporta 'Arrastar e Soltar'.",
 
   /**
    * The text that will be added before the fallback form.
@@ -308,45 +308,45 @@ let defaultOptions = {
    * be ignored.
    */
   dictFallbackText:
-    "Please use the fallback form below to upload your files like in the olden days.",
+    "Por favor, use o formulário abaixo para upar seus arquivos",
 
   /**
    * If the filesize is too big.
    * `{{filesize}}` and `{{maxFilesize}}` will be replaced with the respective configuration values.
    */
   dictFileTooBig:
-    "File is too big ({{filesize}}MiB). Max filesize: {{maxFilesize}}MiB.",
+    "O arquivo é muito grande ({{filesize}}MiB). Tamanho máximo: {{maxFilesize}}MiB.",
 
   /**
    * If the file doesn't match the file type.
    */
-  dictInvalidFileType: "You can't upload files of this type.",
+  dictInvalidFileType: "Você não pode upar arquivos desse tipo.",
 
   /**
    * If the server response was invalid.
    * `{{statusCode}}` will be replaced with the servers status code.
    */
-  dictResponseError: "Server responded with {{statusCode}} code.",
+  dictResponseError: "O servidor respondeu com o código de erro {{statusCode}}.",
 
   /**
    * If `addRemoveLinks` is true, the text to be used for the cancel upload link.
    */
-  dictCancelUpload: "Cancel upload",
+  dictCancelUpload: "Cancelar upload",
 
   /**
    * The text that is displayed if an upload was manually canceled
    */
-  dictUploadCanceled: "Upload canceled.",
+  dictUploadCanceled: "Upload cancelado.",
 
   /**
    * If `addRemoveLinks` is true, the text to be used for confirmation when cancelling upload.
    */
-  dictCancelUploadConfirmation: "Are you sure you want to cancel this upload?",
+  dictCancelUploadConfirmation: "Voc6e tem certeza que deseja cancelar esse upload?",
 
   /**
    * If `addRemoveLinks` is true, the text to be used to remove a file.
    */
-  dictRemoveFile: "Remove file",
+  dictRemoveFile: "Remover arquivo",
 
   /**
    * If this is not null, then the user will be prompted before removing a file.
@@ -357,7 +357,7 @@ let defaultOptions = {
    * Displayed if `maxFiles` is st and exceeded.
    * The string `{{maxFiles}}` will be replaced by the configuration value.
    */
-  dictMaxFilesExceeded: "You can not upload any more files.",
+  dictMaxFilesExceeded: "Você não pode enviar mais nenhum arquivo.",
 
   /**
    * Allows you to translate the different units. Starting with `tb` for terabytes and going down to
